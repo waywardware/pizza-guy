a9cda6fe3cea31189817a97f0662e0e9858bffdc
Adrian Fernandez <afwebs23@gmail.com>
2025-01-15 22:04:19 -0500

    sprite animation - movement
    Implemented movement for sprite

--- player.gd
+++ player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 400
 var screen_size
@@ -6,7 +7,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready(): 
 	screen_size = get_viewport_rect().size
-
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -28,3 +29,11 @@ func _process(delta):
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x !=0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
